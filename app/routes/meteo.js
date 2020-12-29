21c4826608e2bfd81ef78ffd167052c4b0dc3d04
Livonian Order <golybe@gmail.com>
2020-12-30 01:00:51 +0300

    New version of app

--- app/routes/meteo.js
+++ app/routes/meteo.js
@@ -1,8 +1,25 @@
 const {Router} = require('express')
+const landmet = require('../models/landmets')
 const router = Router()
 
 router.get('/', (req,res) => {
     res.render('index')
+})
+
+router.get('/landmet', async (req,res) => {
+    const landmets = await landmet.find({recclass: 'H', name: 'Abo'}).lean()
+    console.log(landmets)
+    res.render('landmet', {
+        landmets
+    })
+})
+
+router.get('/NEO', async (req,res) => {
+  res.render('NEO')
+})
+
+router.get('/MKS', async (req,res) => {
+    res.render('MKS')
 })
 
 module.exports = router
