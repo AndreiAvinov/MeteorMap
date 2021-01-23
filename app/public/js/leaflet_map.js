2a5c964a1a64d93380656283bb07cfbc3fb9eff3
AndreiAvinov <andreiivanchihin.q@ya.ru>
2021-01-23 13:55:41 +0300

    Динамическое обновление маркера

--- app/public/js/leaflet_map.js
+++ app/public/js/leaflet_map.js
@@ -10,25 +10,41 @@ var facts = Array(
 	'Внутренний объём МКС — около тысячи кубических метров пространства.',
 	'МКС — третий по яркости объект на ночном небе после Луны и Венеры.'
 );
+var randomFact = facts[Math.floor(Math.random() * facts.length)]
 
+var map;
+var ISSIcon;
+var ISSmarker;
 
 fetch(url)
 .then(res => res.json())
 .then((out) => {
-	var ISSIcon = L.icon({
+	ISSIcon = L.icon({
 		iconUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/International_Space_Station.svg/320px-International_Space_Station.svg.png',
 		iconSize:     [64, 64], // size of the icon
 		iconAnchor:   [32, 32], // point of the icon which will correspond to marker's location
 		popupAnchor:  [0, -32] // point from which the popup should open relative to the iconAnchor
 	});
-	var map = L.map('map').setView([out.iss_position.latitude, out.iss_position.longitude], 3);
+	map = L.map('map').setView([out.iss_position.latitude, out.iss_position.longitude], 3);
 
 	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 	}).addTo(map);
 
-	L.marker([out.iss_position.latitude, out.iss_position.longitude], {icon: ISSIcon}).addTo(map)
-		.bindPopup(facts[Math.floor(Math.random() * facts.length)])
+	ISSmarker = L.marker([out.iss_position.latitude, out.iss_position.longitude], {icon: ISSIcon}).addTo(map)
+		.bindPopup(randomFact)
 		.openPopup();
+	
 })
 .catch(err => { throw err });
+
+function map_update(){
+	fetch(url)
+	.then(res => res.json())
+	.then((out) => {
+		ISSmarker.setLatLng([out.iss_position.latitude, out.iss_position.longitude])
+	})
+	.catch(err => { throw err });
+}
+
+setInterval(map_update, 5000)
